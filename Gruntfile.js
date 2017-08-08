ed938341cfc93f2d9f5ecd940f9b1bd6ed51f79e
clay973 <sharpclay1@yahoo.com.au>
2017-08-08 09:02:34 +0800

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt) {
+
+    var buildDir = 'build/';
+
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+
+        build: {
+            options: {
+                buildDir: buildDir,
+                srcDir: 'src/',
+                assetsHome: 'assets/',
+                checkDeps: true,
+                depsDir: 'vendors/'
+                /*cdn: {
+                    hosts: {
+                        'css': ['https://css1.clearwallet.dev', 'https://css2.clearwallet.dev'],
+                        'js': ['https://js1.clearwallet.dev', 'https://js2.clearwallet.dev'],
+                        'assets': ['https://assets1.clearwallet.dev', 'https://assets2.clearwallet.dev']
+                    }
+                }*/
+            },
+            process: {
+                files: [
+                    {cwd: 'src/', src: 'index.html', dest: buildDir, expand: true},
+                    {cwd: 'src/pages/', src: '*.html', dest: buildDir+'pages/', expand: true},
+                    {cwd: 'src/locales/en', src: '*.json', dest: buildDir+'locales/en', expand: true}
+                ]
+            },
+            copy: {
+                files: [
+                    {src: 'src/robots.txt', dest: buildDir+'robots.txt'},
+                    {cwd: 'src/assets/', src: '*', dest: buildDir+'assets/', expand: true}
+                ]
+            }
+        },
+        transifex: {
+            languages: ['fr', 'de', 'ar', 'zh', 'zh_TW', 'it', 'es', 'he']
+        }
+    }
+    /*config['chrome-extension'] = {
+        options: {
+            name: "clearwallet",
+            version: pkg.version,
+            id: "00000000000000000000000000000000",
+            //updateUrl: "http://example.com/extension/111111/",
+            chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+            clean: true,
+            certDir: '.',
+            buildDir: '.',
+            resources: [
+                "build/**"
+            ]
+        }
+    }*/
+    
+    grunt.initConfig(config);
+
+    grunt.loadTasks('grunt-tasks');
+
+    grunt.registerTask('default', ['build']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# ClearwalletPKRecovery
-Clearwallet Private Key Recovery site
+Clearwallet
+================
+
+Online Webwallet for [ClearingHouse](http://clearinghouse.io).
+
+Originally based off of [Carbonwallet](http://www.carbonwallet.com) (however virtually all the original code has been removed or rewritten).
+
+
+Production Systems
+-------------------
+
+* Mainnet: **[clearwallet.co](https://clearwallet.co/)**
+* Testnet: **[testnet.clearwallet.co](https://testnet.clearwallet.co/)**
+
+
+Features
+----------
+
+- Deterministic wallet addresses (BIP 32-based)
+- Supports the majority of ClearingHouse functionality
+- Fully-AJAX driven
+- Anonymous
+- Runs in the browser, with keys created in memory
+
+
+Browser Support
+-------------------
+
+**Desktop**
+
+- Chrome 23+ (Preferred)
+- Firefox 25+
+- Safari 7+
+- Opera 15+
+
+Notably, Internet Explorer is **not** supported, due to its lack of full Content-Security-Policy support (even with IE 11).
+
+**Mobile**
+
+- IOS Safari 7+
+- Android Browser 4.4+
+- Chrome for Android 33+
+- Chrome for iOS 35+
+- Firefox for Android 26+
+
+
+Build Instructions
+-------------------
+
+### Before running the build system:
+```
+sudo npm install -g grunt-cli bower
+```
+
+### To build:
+```
+cd src; bower install; cd ..
+npm install
+```
+
+### To (re)build the static (i.e. minified) site:
+```
+grunt build
+```
+
+### To regenerate dependencies hash file (```src/.bowerhashes```):
+```
+grunt freeze
+```
+
+Setting up your own Clearwallet Server
+-----------------------------------------
+
+See [this link](https://github.com/ClearingHouse/clearinghoused_build/blob/master/docs/SettingUpAFederatedNode.rst) for more info.
+
+
+Licence
+-------------------
+
+http://opensource.org/licenses/CDDL-1.0
